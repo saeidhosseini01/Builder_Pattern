55b5c9f75f45ee5c11b78cf1b1f28b7fc97f8882
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2024-08-06 23:08:53 +0430

    createClasses

---
+++ Builder_Pattern/Builder/Builder.cs
@@ -0,0 +1,24 @@
+﻿using Builder_Pattern.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Pattern.Builder
+{
+    public abstract class Builder
+    {
+        protected Product product;
+        public Builder(Product product)
+        {
+            this.product = product;
+        }
+        public abstract void BuilderPart1();
+        public abstract void BuilderPart2();
+        public virtual Product Getresult()
+        {
+            return product;
+        }
+    }
+}
---
+++ Builder_Pattern/Classes/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Pattern.Classes
+{
+    public class Product
+    {
+        public string Part1 { get; set; }
+        public string Part2 { get; set; }
+        public string Name { get; set; }
+
+        public void Displayinfo()
+        {
+            Console.WriteLine($"this Part1 {Part1}");
+            Console.WriteLine($"this Part2 {Part2}");
+            Console.WriteLine($"this Name {Name}");
+        }
+    }
+}
