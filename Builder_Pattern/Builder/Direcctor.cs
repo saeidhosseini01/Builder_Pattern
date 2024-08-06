88569b44da948a08848c9f181c1bcf1d121da75f
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2024-08-06 23:32:29 +0430

    CreateDirectorClass

---
+++ Builder_Pattern/Builder/Direcctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Pattern.Builder
+{
+    public class Direcctor
+    {
+        private Builder builder;
+        public void DetBuilder( Builder builder )
+        {
+              this.builder=builder;
+        }
+        public Direcctor()
+        {
+            builder.BuildPart1();
+            builder.BuildPart2();
+        }
+    }
+}
